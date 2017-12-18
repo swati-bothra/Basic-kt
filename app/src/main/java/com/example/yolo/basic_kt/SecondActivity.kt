8e1e4a4f010cf41aeba919052c62e57f66e5a919
YoloHealth <jainswati048@gmail.com>
2017-12-18 16:33:37 +0530

    Now No findViewById

---
+++ app/src/main/java/com/example/yolo/basic_kt/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.yolo.basic_kt
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by yolo on 18/12/17.
+ */
+class SecondActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
