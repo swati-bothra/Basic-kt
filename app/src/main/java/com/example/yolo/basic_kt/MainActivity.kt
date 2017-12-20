054ce33d385e5e4006c673bfd7ae2e1e722f5f4a
YoloHealth <jainswati048@gmail.com>
2017-12-20 17:31:08 +0530

    Google sign with kotlin

--- app/src/main/java/com/example/yolo/basic_kt/MainActivity.kt
+++ app/src/main/java/com/example/yolo/basic_kt/MainActivity.kt
@@ -2,13 +2,46 @@ package com.example.yolo.basic_kt
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),GoogleApiClient.OnConnectionFailedListener{
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Toast.makeText(this,"ConnectionFailed!!",Toast.LENGTH_LONG)
+        Log.e("ConnectionFailed",p0.toString())
+    }
+
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView1.setText("Hiii There!!!")
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
